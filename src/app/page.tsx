ff49492cb80ae017ded92d542a78a32caa34ea92
Joy <baterdeneorgil22@gmail.com>
2023-05-22 14:59:52 +0800

    cock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import Card from '@/components/card'
 import Image from 'next/image'
 
 export default function Home() {
     return (
         <>
             <div className='w-full h-screen bg-black'>
-                <div className='w-full px-8 py-4 bg-gray-500'>
+                <div className='w-full px-8 py-4 bg-[#2B2B2B]'>
                     <img src='https://playmo.mn/img/logo_white.svg' className='w-32'></img>
                 </div>
                 <img src='https://api.playmo.mn/storage/images/media/banner/fa2XZtqZmgovgw9ppaLD7A17FUvvJFCNRzf5r5LK.jpg'></img>
+                <div className='text-3xl font-bold p-2'>
+                Үзэлт ихтэй
+                <Card/>
+                </div>
             </div>
         </>
     )
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,11 @@
-export default function Card() {
+export default function Card(data:any) {
     return(
         <>
+        <div>
+            {data.backdrop_path}
+            {data.release_date}
+            {data.title}
+        </div>
+        </>
     )
 }
